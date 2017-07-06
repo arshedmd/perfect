ae38f3650e294b50941c706930f4e4415bbdb632
kctech <kctech@kctech-PC>
2017-07-06 14:25:05 +0530

    first commit

---
+++ Object Repository/perfectoneObj1.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>perfectoneObj1</name>
+   <tag></tag>
+   <elementGuidId>8d93936e-8dd5-456c-881b-8c4c8d506751</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>reer</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>perfectoneObj2</name>
+      <type>Main</type>
+      <value></value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/perfectoneObj2.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>perfectoneObj2</name>
+   <tag></tag>
+   <elementGuidId>9ebdfea3-c9cb-4161-81fa-694df603a9e4</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>xccc</value>
+   </webElementProperties>
+</WebElementEntity>
